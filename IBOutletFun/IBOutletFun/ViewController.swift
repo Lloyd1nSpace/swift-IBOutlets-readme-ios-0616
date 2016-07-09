77a1a95959b4dc46473aa90c6967ce4899ba2009
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-09 02:28:31 -0700

    Can never go over topics like these enought

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(sender: AnyObject) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello, world!"
+        
+    }
+    
+    @IBAction func goodbyWorld(sender: AnyObject) {
+        print("Pressed Goodbye World button")
+        textDisplay.text = "Goodbye world!"
+    }
+    
+    @IBAction func unicorn(sender: AnyObject) {
+        print("Pressed 🦄 button")
+        textDisplay.text = "🦄"
+        // Can use emoji in strings in Swift!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textDisplay.text = "Zzz..."
         // Do any additional setup after loading the view, typically from a nib.
     }
 
